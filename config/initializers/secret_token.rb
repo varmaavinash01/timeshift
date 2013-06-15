fe48aa423e8c21e2392d544589022effebf70991
varmaavinash01 <avinash.varma4464@gmail.com>
2013-06-15 10:07:36 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timeshift::Application.config.secret_token = '30634c5252736954bb8bbdc7f34f2643dd4b673fbeee1464875e7e142e74e71e7a3593a9248cdc6990af9afb1bed4abcda7a8015c56e9a472b5a623a606f2ba0'
